116feb7fe531051602ba847c57ae634eed388ff2
Daniel <dansto13@yahoo.com>
2019-05-06 23:09:02 +0200

    Implement change requests.

--- src/components/layout/SignedInLinks.js
+++ src/components/layout/SignedInLinks.js
@@ -1,22 +1,20 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
 
-const SignedInLinks = () => {
-    return (
-        <ul className='right'>
-            <li>
-                <NavLink to='/'>New Retro</NavLink>
-            </li>
-            <li>
-                <NavLink to='/'>Log Out</NavLink>
-            </li>
-            <li>
-                <NavLink to='/' className='btn btn-floating pink lighten-1'>
-                    DS
-                </NavLink>
-            </li>
-        </ul>
-    );
-};
+const SignedInLinks = () => (
+    <ul className="right">
+        <li>
+            <NavLink to="/">New Retro</NavLink>
+        </li>
+        <li>
+            <NavLink to="/">Log Out</NavLink>
+        </li>
+        <li>
+            <NavLink to="/" className="btn btn-floating pink lighten-1">
+                DS
+            </NavLink>
+        </li>
+    </ul>
+);
 
 export default SignedInLinks;
--- src/components/layout/SignedOutLinks.js
+++ src/components/layout/SignedOutLinks.js
@@ -1,17 +1,15 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
 
-const SignedOutLinks = () => {
-    return (
-        <ul className='right'>
-            <li>
-                <NavLink to='/'>Sign Up</NavLink>
-            </li>
-            <li>
-                <NavLink to='/'>Login</NavLink>
-            </li>
-        </ul>
-    );
-};
+const SignedOutLinks = () => (
+    <ul className="right">
+        <li>
+            <NavLink to="/">Sign Up</NavLink>
+        </li>
+        <li>
+            <NavLink to="/">Login</NavLink>
+        </li>
+    </ul>
+);
 
 export default SignedOutLinks;
