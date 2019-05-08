56bbfc929633782e437d667a43137958ac5ea257
Daniel <dansto13@yahoo.com>
2019-05-08 23:30:57 +0200

    Added route for Dashboard.

--- src/components/dashboard/Dashboard.js
+++ src/components/dashboard/Dashboard.js
@@ -4,15 +4,19 @@ import RetrosList from '../retros/RetrosList';
 
 class Dashboard extends Component {
     render() {
-        <div className="dashboard container">
-            <div className="row">
-                <div className="col s12 m6">
-                    <RetrosList />
-                </div>
-                <div className="col s12 m5 offset-m1">
-                    <Notifications />
+        return (
+            <div className="dashboard container">
+                <div className="row">
+                    <div className="col s12 m6">
+                        <RetrosList />
+                    </div>
+                    <div className="col s12 m5 offset-m1">
+                        <Notifications />
+                    </div>
                 </div>
             </div>
-        </div>;
+        );
     }
 }
+
+export default Dashboard;
--- src/components/retros/RetrosList.js
+++ src/components/retros/RetrosList.js
@@ -1,27 +1,31 @@
 import React from 'react';
 
 const RetrosList = () => {
-    <div className="retros-list section">
-        <div className="card z-depth-1 retro-summary">
-            <div className="card-content grey-text text-darken-3">
-                <span className="card-title">Retro Title</span>
-                <p>Added by the Scrum Master</p>
-                <p className="grey-text">May 13th, 2 p.m.</p>
+    return (
+        <div className="retros-list section">
+            <div className="card z-depth-1 retro-summary">
+                <div className="card-content grey-text text-darken-3">
+                    <span className="card-title">Retro Title</span>
+                    <p>Added by the Scrum Master</p>
+                    <p className="grey-text">May 13th, 2 p.m.</p>
+                </div>
+            </div>
+            <div className="card z-depth-1 retro-summary">
+                <div className="card-content grey-text text-darken-3">
+                    <span className="card-title">Retro Title</span>
+                    <p>Added by the Scrum Master</p>
+                    <p className="grey-text">May 13th, 2 p.m.</p>
+                </div>
+            </div>
+            <div className="card z-depth-1 retro-summary">
+                <div className="card-content grey-text text-darken-3">
+                    <span className="card-title">Retro Title</span>
+                    <p>Added by the Scrum Master</p>
+                    <p className="grey-text">May 13th, 2 p.m.</p>
+                </div>
             </div>
         </div>
-        <div className="card z-depth-1 retro-summary">
-            <div className="card-content grey-text text-darken-3">
-                <span className="card-title">Retro Title</span>
-                <p>Added by the Scrum Master</p>
-                <p className="grey-text">May 13th, 2 p.m.</p>
-            </div>
-        </div>
-        <div className="card z-depth-1 retro-summary">
-            <div className="card-content grey-text text-darken-3">
-                <span className="card-title">Retro Title</span>
-                <p>Added by the Scrum Master</p>
-                <p className="grey-text">May 13th, 2 p.m.</p>
-            </div>
-        </div>
-    </div>;
+    );
 };
+
+export default RetrosList;
