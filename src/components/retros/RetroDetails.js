9ec354c680e4e3484ecc149aafec06f97113aa67
Daniel <dansto13@yahoo.com>
2019-07-05 15:19:25 +0200

    Migrate retros to Bootstrap.

--- src/components/retros/RetroDetails.js
+++ src/components/retros/RetroDetails.js
@@ -1,27 +1,41 @@
 import React from 'react';
+import {
+    Container,
+    Card,
+    CardBody,
+    CardTitle,
+    CardText,
+    CardSubtitle,
+    CardFooter
+} from 'reactstrap';
 import PropTypes from 'prop-types';
 
 const RetroDetails = props => {
     const id = props.match.params.id;
     return (
-        <div className="container section project-details">
-            <div className="card">
-                <div className="card-content">
-                    <span className="card-title">Retro Title #{id} </span>
-                    <p>
+        <Container className="retro-details">
+            <Card>
+                <CardBody>
+                    <CardTitle>Retro Title #{id}</CardTitle>
+                    <CardText>
                         Lorem ipsum dolor sit amet consectetur adipisicing elit.
                         Perspiciatis velit ab optio eius corporis iste facilis,
                         sit eum distinctio inventore repudiandae obcaecati
                         eveniet sunt, quisquam iure quaerat amet reiciendis
                         error?
-                    </p>
-                </div>
-                <div className="card-action grey lighten-4 grey-text">
+                    </CardText>
+                </CardBody>
+                <CardFooter
+                    style={{
+                        backgroundColor: 'lightgray',
+                        borderColor: 'lightgray'
+                    }}
+                >
                     <div>Posted by Daniel</div>
                     <div>May 13th, 2am</div>
-                </div>
-            </div>
-        </div>
+                </CardFooter>
+            </Card>
+        </Container>
     );
 };
 
