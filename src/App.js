2edee05238e46bcd5df82a7a345051a139d67f02
Daniel <dansto13@yahoo.com>
2019-07-05 13:51:30 +0200

    Migrate navigation to Bootstrap.

--- src/App.js
+++ src/App.js
@@ -1,19 +1,18 @@
 import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
-import Navbar from './components/layout/Navbar';
+import Navigation from './components/layout/Navigation';
 import Dashboard from './components/dashboard/Dashboard';
 import Login from './components/auth/Login';
 import Register from './components/auth/Register';
 import RetroDetails from './components/retros/RetroDetails';
 import AddRetro from './components/retros/AddRetro';
 
-
 function App() {
     return (
         <BrowserRouter>
             <div className="App">
-                <Navbar />
+                <Navigation />
                 <Switch>
                     <Route exact path="/" component={Dashboard} />
                     <Route path="/login" component={Login} />
---
+++ src/components/layout/Navigation.js
@@ -0,0 +1,45 @@
+import React, { Component } from 'react';
+import {
+    Collapse,
+    Navbar,
+    NavbarToggler,
+    NavbarBrand,
+    Nav,
+    Container
+} from 'reactstrap';
+
+import SignedInLinks from './SignedInLinks';
+import SignedOutLinks from './SignedOutLinks';
+
+class Navigation extends Component {
+    state = {
+        isOpen: false
+    };
+
+    toggle = () => {
+        this.setState({
+            isOpen: !this.state.isOpen
+        });
+    };
+
+    render() {
+        return (
+            <div>
+                <Navbar color="dark" dark expand="sm" className="mb-5">
+                    <Container>
+                        <NavbarBrand href="/">Retrify</NavbarBrand>
+                        <NavbarToggler onClick={this.toggle} />
+                        <Collapse isOpen={this.state.isOpen} navbar>
+                            <Nav className="ml-auto" navbar>
+                                <SignedOutLinks />
+                                <SignedInLinks />
+                            </Nav>
+                        </Collapse>
+                    </Container>
+                </Navbar>
+            </div>
+        );
+    }
+}
+
+export default Navigation;
