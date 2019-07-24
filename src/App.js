b69cac49fe27a47d54e1701dc10a1ef90c86fd72
Daniel <dansto13@yahoo.com>
2019-07-24 11:26:12 +0200

    Setup React-Redux.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import { Provider } from 'react-redux';
 
 import Navigation from './components/layout/Navigation';
 import Dashboard from './components/dashboard/Dashboard';
@@ -15,18 +16,20 @@ console.log(store.getState());
 
 function App() {
     return (
-        <BrowserRouter>
-            <div className="App">
-                <Navigation />
-                <Switch>
-                    <Route exact path="/" component={Dashboard} />
-                    <Route path="/login" component={Login} />
-                    <Route path="/register" component={Register} />
-                    <Route path="/retro/:id" component={RetroDetails} />
-                    <Route path="/retro" component={AddRetro} />
-                </Switch>
-            </div>
-        </BrowserRouter>
+        <Provider store={store}>
+            <BrowserRouter>
+                <div className="App">
+                    <Navigation />
+                    <Switch>
+                        <Route exact path="/" component={Dashboard} />
+                        <Route path="/login" component={Login} />
+                        <Route path="/register" component={Register} />
+                        <Route path="/retro/:id" component={RetroDetails} />
+                        <Route path="/retro" component={AddRetro} />
+                    </Switch>
+                </div>
+            </BrowserRouter>
+        </Provider>
     );
 }
 
