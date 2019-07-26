a50cad782bae4b8f2ecb9ed9c7376a0dc7bdbfd0
Daniel <dansto13@yahoo.com>
2019-07-26 20:09:40 +0200

    Add tests for retros action generators.

---
+++ src/tests/actions/retros.test.js
@@ -0,0 +1,60 @@
+import uuid from 'uuid';
+import { addRetro, editRetro, removeRetro } from '../../actions/retros';
+
+describe('Retros action generator', () => {
+    it('should generate addRetro action object with provided values', () => {
+        const retro = {
+            title: 'The retro',
+            author: '',
+            createdAt: 0,
+            id: uuid()
+        };
+        const action = addRetro(retro);
+
+        expect(action).toEqual({
+            type: 'ADD_RETRO',
+            retro: {
+                ...retro,
+                id: expect.any(String)
+            }
+        });
+    });
+
+    it('should generate addRetro action object with default values', () => {
+        const action = addRetro();
+
+        expect(action).toEqual({
+            type: 'ADD_RETRO',
+            retro: {
+                id: expect.any(String),
+                title: '',
+                author: '',
+                createdAt: 0
+            }
+        });
+    });
+
+    it('should generate removeRetro action object', () => {
+        const id = '123';
+        const action = removeRetro(id);
+        expect(action).toEqual({
+            type: 'REMOVE_RETRO',
+            id
+        });
+    });
+
+    it('should generate editRetro action object', () => {
+        const id = '123';
+        const updates = {
+            title: 'Modified',
+            author: 'Daniel',
+            createdAt: 2000
+        };
+        const action = editRetro(id, updates);
+        expect(action).toEqual({
+            type: 'EDIT_RETRO',
+            id,
+            updates
+        });
+    });
+});
