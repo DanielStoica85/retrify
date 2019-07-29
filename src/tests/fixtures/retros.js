ea051a1a2560bb6c3fa3c493a670bfbc90172b81
Daniel <dansto13@yahoo.com>
2019-07-29 12:39:59 +0200

    Add retros fixtures.

---
+++ src/tests/fixtures/retros.js
@@ -0,0 +1,26 @@
+export default [
+    {
+        id: 1,
+        title: 'First Retro',
+        author: 'Gica',
+        description:
+            'Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti itaque aut eligendi laborum excepturi animi hic similique impedit enim velit.',
+        createdAt: 1564169537000
+    },
+    {
+        id: 2,
+        title: 'Second Retro',
+        author: 'Costel',
+        description:
+            'Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti itaque aut eligendi laborum excepturi animi hic similique impedit enim velit.',
+        createdAt: 1564255937000
+    },
+    {
+        id: 3,
+        title: 'Third Retro',
+        author: 'Miron',
+        description:
+            'Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti itaque aut eligendi laborum excepturi animi hic similique impedit enim velit.',
+        createdAt: 1564342337000
+    }
+];
