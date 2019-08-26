b8c5e86a5d45f7dfbcca9e0bc2d2f35d5865befd
Daniel <dansto13@yahoo.com>
2019-08-26 21:45:12 +0200

    Add async action for adding retro.

--- src/actions/retros.js
+++ src/actions/retros.js
@@ -17,6 +17,32 @@ export const addRetro = ({
     }
 });
 
+// add retro to db and then to store
+export const startAddRetro = (retroData = {}) => {
+    return dispatch => {
+        const {
+            title = '',
+            description = '',
+            createdAt = 0,
+            author = ''
+        } = retroData;
+
+        const retro = { title, description, createdAt, author };
+
+        return database
+            .ref('retros')
+            .push(retro)
+            .then(ref => {
+                dispatch(
+                    addRetro({
+                        id: ref.key,
+                        ...retro
+                    })
+                );
+            });
+    };
+};
+
 // Remove retro
 export const removeRetro = id => ({
     type: 'REMOVE_RETRO',
