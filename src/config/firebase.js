86ca9961a418331c02ccd0cd773c40a87883e816
Daniel <dansto13@yahoo.com>
2019-08-26 23:47:51 +0200

    Remove key.

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -2,15 +2,7 @@ import firebase from 'firebase/app';
 import 'firebase/database';
 
 // Firebase configuration
-const firebaseConfig = {
-    apiKey: 'AIzaSyAQrMZkRE2DsK2Fu5xr8aNJtiWRU84tLOU',
-    authDomain: 'retrify-f23eb.firebaseapp.com',
-    databaseURL: 'https://retrify-f23eb.firebaseio.com',
-    projectId: 'retrify-f23eb',
-    storageBucket: '',
-    messagingSenderId: '543816546735',
-    appId: '1:543816546735:web:a206de2ebd8b8195'
-};
+const firebaseConfig = {};
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
 
